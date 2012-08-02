7fd14d41ee24b08ee2a06f3dcf30a8e1f7067246
Kevin Swiber <kswiber@gmail.com>
2012-08-02 15:55:14 -0400

    Refactoring for cleaner API.  Including a property transformation on the where function.

--- clarify.js
+++ clarify.js
@@ -1,77 +1,89 @@
 (function() {
-  (function ensureCompatibility() { 
-    // Array.prototype.filter compatibility taken from:
-    //    https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/filter
-    if (!Array.prototype.filter) {
-      Array.prototype.filter = function(fun /*, thisp */) {
-        "use strict";
-
-        if (this == null) {
-          throw new TypeError();
-        }
-
-        var t = Object(this);
-        var len = t.length >>> 0;
-        if (typeof fun != 'function') {
-          throw new TypeError();
-        }
-
-        var res = [];
-        var thisp = arguments[1];
-
-        for (var i = 0; i < len; i++) {
-          if (i in t) {
-            var val = t[i]; // in case fun mutates this
-            if (fun.call(thisp, val, i, t)) {
-              res.push(val);
-            }
-          }
-        }
-      };
-    }
-  })();
-
-  function Query(record) {
-    this.record = record;
-    this.filter = null;
-    this.state = state.initialized;
+  var state = {
+    initialized: 0,
+    filterProperty: 1,
+    propertyTransformation: 2,
+    filterAction: 3,
+    done: 4
   }
 
-  Query.prototype.where = function(prop) {
-    this.state = state.filterProperty;
+  /* Query
+   */
+  function Query(record) {
+    if (Object.prototype.toString.call(record) !== '[object Array]') {
+      throw new TypeError();
+    }
+
+    this.record = record;
+    this.filter = null;
+    this._transition(state.initialized);
+  }
+
+  Query.prototype.where = function(prop, transformation) {
+    this._transition(state.filterProperty);
+
     this.filter = new Filter(this);
     this.filter.property = prop;
+
+    if (transformation) {
+      this.filter.transformation = function(prop) {
+        this.query.state = state.propertyTransformation;
+        transformation(prop);
+      };
+    }
 
     return this.filter;
   };
 
+  Query.prototype._transition = function(state) {
+    //console.log('moving from %s to %s', this.state, state);
+    this.state = state;
+  };
+
+  /* Filter
+   */
   function Filter(query) {
     this.query = query;
     this.property = '';
+    this.transformation = function(obj) {};
     this.action = function(obj) {};
   }
 
+  Filter.prototype.run = function() {
+    var record = this.query.record;
+
+    if (this.transformation) {
+      this.query._transition(state.propertyTransformation);
+      record = record.map(this._applyTransformation);
+    }
+
+    this.query._transition(state.filterAction);
+    var res = this.action(record);
+
+    this.query._transition(state.done);
+
+    return res;
+  }
+
   Filter.prototype.satisfies = function(predicate) {
+    if (typeof predicate !== 'function') {
+      throw new TypeError();
+    }
+
     this.action = function(obj) {
-      if (Object.prototype.toString.call(obj) !== '[object Array]') {
-        throw new TypeError();
-      }
-      
-      var res = obj.filter(function(item) { 
-        if (predicate(item)) {
-          return item;
+      var res = [];
+      var len = obj.length >>> 0;
+
+      for(var i = 0; i < len; i++) {
+        if (predicate(obj[i])) {
+          res.push(obj[i]);
         }
-      });
-      
+      }
+
       return res;
     };
 
-    this.query.state = state.filterAction;
-
-    var res = this.action(this.query.record);
-    this.state = state.done;
-
-    return res;
+    return this.run();
   };
 
   Filter.prototype.contains = function(val) {
@@ -84,13 +96,16 @@
     return that.satisfies(function(obj) { return obj[that.property] === val; });
   };
 
-  var state = {
-    initialized: 0,
-    filterProperty: 1,
-    filterAction: 2,
-    done: 3
-  }
+  Filter.prototype._applyTransformation = function(item) {
+    if (this.property in item && item[this.property] && this.transformation) {
+      item[this.property] = this.transformation(item[this.property]);
+    }
 
+    return item;
+  };
+
+  /* Clarify
+   */
   function Clarify() { }
 
   Clarify.select = function(record) { 
---
+++ example.js
@@ -0,0 +1,21 @@
+var clarify = require('./');
+var select = clarify.select;
+var order = require('./order');
+
+var customer =
+  select(order.entities)
+  .where('rel', split(/\s/))
+  .contains('http://x.io/rels/customer')
+  .shift();
+
+var link = 
+  select(customer.links)
+  .where('rel', split(/\s/))
+  .contains('self')
+  .shift();
+
+console.log(link);
+
+function split(delimiter) {
+  return function(s) { return s.split(delimiter); };
+}
